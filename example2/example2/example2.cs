d27d53a6c2e1473c5dc4a3101e91c0a80e9097e3
Shaulenkov <95738324+Shaulenkov@users.noreply.github.com>
2021-12-10 11:26:50 +0300

    Add files via upload

---
+++ example2/example2/example2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Урок_1
+{
+    class Example2
+    {   /*  Ввести вес и рост человека. Рассчитать и вывести 
+    индекс массы тела (ИМТ) по формуле I=m/(h*h); 
+    где m — масса тела в килограммах, h — рост в метрах.  */
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Введите ваш вес");
+            double weight = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите ваш рост");
+            double height = Double.Parse(Console.ReadLine());
+
+            height /= 100;
+            
+            
+
+            Console.WriteLine("Индекс вашего тела = ");
+            double mass = weight / (height * height);
+            Console.WriteLine("индекс массы тела = ", mass);
+
+        }
+    }
+}
